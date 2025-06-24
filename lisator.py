9710fc3ed7f0c320b44151b90f4149b214342e6a
megahomyak <g.megahomyak@gmail.com>
2025-06-24 08:21:01 +0500

    Implemented the back generator as well. Now it's very neat and actually works both sides

--- lisator.py
+++ lisator.py
@@ -2,37 +2,41 @@ import sys
 import os
 import json
 
-file_name = sys.argv[1]
+dir_name = sys.argv[1]
 for dir_ in ["compiled", "rendered"]:
-    os.makedirs(dir_, exist_ok=True)
+    os.makedirs(f"{dir_}/{dir_name}", exist_ok=True)
 PAGEWIDTH_MM = 210
 PAGEHEIGHT_MM = 297
-j = json.load(open(f"foxes/{file_name}.json"))
+j = json.load(open(f"foxes/{dir_name}/pattern.json"))
 cols = j["cols"]
 rows = j["rows"]
 del j
-with open(f"compiled/{file_name}.svg", "w") as f:
-    class tag:
-        def __init__(self, name, attrs=None):
-            self._name = name
-            self._attrs = " "+attrs if attrs else ""
-        def add(self):
-            f.write(f"<{self._name}{self._attrs} />")
-        def __enter__(self, *_, **__):
-            f.write(f"<{self._name}{self._attrs}>")
-        def __exit__(self, *_, **__):
-            f.write(f"</{self._name}>")
-    f.write('<?xml version="1.0" encoding="UTF-8"?>')
-    with tag("svg", f'width="{PAGEWIDTH_MM}mm" height="{PAGEHEIGHT_MM}mm" version="1.1" viewBox="0 0 {PAGEWIDTH_MM} {PAGEHEIGHT_MM}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"'):
-        with tag("defs"):
-            foxwidth = PAGEWIDTH_MM / cols
-            foxheight = PAGEHEIGHT_MM / rows
-            with tag("pattern", f'id="foxPattern" patternUnits="userSpaceOnUse" width="{foxwidth}" height="{foxheight}"'):
-                tag("image", f'xlink:href="{os.getcwd()}/foxes/{file_name}" width="{foxwidth}" height="{foxheight}" preserveAspectRatio="none"').add()
-        tag("rect", f'width="100%" height="100%" fill="url(#foxPattern)"').add()
+def generate(side):
+    with open(f"compiled/{dir_name}/{side}.svg", "w") as f:
+        class tag:
+            def __init__(self, name, attrs=None):
+                self._name = name
+                self._attrs = " "+attrs if attrs else ""
+            def add(self):
+                f.write(f"<{self._name}{self._attrs} />")
+            def __enter__(self, *_, **__):
+                f.write(f"<{self._name}{self._attrs}>")
+            def __exit__(self, *_, **__):
+                f.write(f"</{self._name}>")
+        f.write('<?xml version="1.0" encoding="UTF-8"?>')
+        with tag("svg", f'width="{PAGEWIDTH_MM}mm" height="{PAGEHEIGHT_MM}mm" version="1.1" viewBox="0 0 {PAGEWIDTH_MM} {PAGEHEIGHT_MM}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"'):
+            if side == "back":
+                tag("rect", f'width="100%" height="100%" fill="white"').add()
+            with tag("defs"):
+                foxwidth = PAGEWIDTH_MM / cols
+                foxheight = PAGEHEIGHT_MM / rows
+                with tag("pattern", f'id="foxPattern" patternUnits="userSpaceOnUse" width="{foxwidth}" height="{foxheight}"'):
+                    imgpath = f"foxes/{dir_name}/image.png" if side == "front" else "sticker_back.svg"
+                    preserveAspectRatio = "" if side == "back" else ' preserveAspectRatio="none"'
+                    tag("image", f'xlink:href="{os.getcwd()}/{imgpath}" width="{foxwidth}" height="{foxheight}"{preserveAspectRatio}').add()
+            tag("rect", f'width="100%" height="100%" fill="url(#foxPattern)"').add()
+generate("front")
+generate("back")
 
-os.system(f"inkscape --export-type=png --export-filename=rendered/{file_name} --export-dpi=600 compiled/{file_name}.svg")
-FOXES_BACK_INPUT = "foxes_back.svg"
-FOXES_BACK_OUTPUT = "foxes_back.png"
-if not os.path.exists(FOXES_BACK_OUTPUT):
-    os.system(f"inkscape --export-type=png --export-filename={FOXES_BACK_OUTPUT} --export-dpi=300 {FOXES_BACK_INPUT}")
+os.system(f"inkscape --export-type=png --export-filename=rendered/{dir_name}/front.png --export-dpi=600 compiled/{dir_name}/front.svg")
+os.system(f"inkscape --export-type=png --export-filename=rendered/{dir_name}/back.png --export-dpi=300 compiled/{dir_name}/back.svg")
